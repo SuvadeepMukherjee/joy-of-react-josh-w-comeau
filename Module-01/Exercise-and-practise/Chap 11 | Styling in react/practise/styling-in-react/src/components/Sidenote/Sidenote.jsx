6f5e8b44cb4e2a356d74b61a04e5b68e2a27c2ac
suvadeep <suvadeepm0709@gmail.com>
2025-10-14 14:10:25 +0530

    practise exercise on styling

---
+++ Module-01/Exercise-and-practise/Chap 11 | Styling in react/practise/styling-in-react/src/components/Sidenote/Sidenote.jsx
@@ -0,0 +1,14 @@
+import styles from './Sidenote.module.css';
+
+console.log(styles);
+
+function Sidenote({ title, children }) {
+  return (
+    <aside className={styles.wrapper}>
+      <h3 className={styles.title}>{title}</h3>
+      <p>{children}</p>
+    </aside>
+  );
+}
+
+export default Sidenote;
